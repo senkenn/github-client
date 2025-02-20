a152fb25a26e8c61ea354d7ee6373e208219c3bb
senkenn <senken32@gmail.com>
2025-02-21 01:12:03 +0900

    feat: add Tailwind CSS integration and update HTML structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+  ],
 });
