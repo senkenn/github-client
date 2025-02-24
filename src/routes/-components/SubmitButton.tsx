8c65a45a9497a55f2ce4220592fd1c8b59cd4c10
senkenn <senken32@gmail.com>
2025-02-25 03:24:46 +0900

    feat: update SubmitButton component to accept custom className and enhance form layout

--- src/routes/-components/SubmitButton.tsx
+++ src/routes/-components/SubmitButton.tsx
@@ -1,12 +1,12 @@
 import { useFormStatus } from "react-dom";
 
-export function SubmitButton() {
+export function SubmitButton({ className }: { className?: string }) {
   const { pending } = useFormStatus();
   return (
     <button
       type="submit"
       disabled={pending}
-      className="bg-blue-500 text-white p-2 rounded"
+      className={`bg-blue-500 text-white p-2 rounded ${className}`}
     >
       {pending ? "Fetching..." : "Fetch Comments"}
     </button>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -67,39 +67,38 @@ function Index() {
   const [{ owner, repo, issueNumber, result }, action] = useActionState(
     fetchCommentsAction,
     {
-      owner: "senkenn",
-      repo: "sqlsurge",
-      issueNumber: "1",
+      owner: "",
+      repo: "",
+      issueNumber: "",
       result: { isOk: true, issueComments: [] },
     }
   );
 
   return (
     <>
-      {/** TODO: もっと見た目良く */}
-      <form action={action} className="p-2">
+      <form action={action} className="p-2 flex flex-wrap gap-2 items-center">
         <input
           type="text"
           placeholder="Enter Owner (e.g. user)"
-          className="border border-gray-300 rounded p-2 mb-4 w-full"
+          className="border border-gray-300 rounded p-2 mb-2 flex-grow"
           name="owner"
           defaultValue={owner}
         />
         <input
           type="text"
           placeholder="Enter Repo (e.g. repo)"
-          className="border border-gray-300 rounded p-2 mb-4 w-full"
+          className="border border-gray-300 rounded p-2 mb-2 flex-grow"
           name="repo"
           defaultValue={repo}
         />
         <input
           type="text"
           placeholder="Enter Issue Number (e.g. 1)"
-          className="border border-gray-300 rounded p-2 mb-4 w-full"
+          className="border border-gray-300 rounded p-2 mb-2 flex-grow"
           name="issueNumber"
           defaultValue={issueNumber}
         />
-        <SubmitButton />
+        <SubmitButton className="flex-grow mb-2" />
       </form>
 
       {/* Display comments */}
