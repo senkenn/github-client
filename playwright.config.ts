291a4a05d936b3a464d2d4aa5d61dc10626fcec7
senkenn <senken32@gmail.com>
2025-08-05 01:31:18 +0900

    feat: add screenshot comparison settings to Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,6 +16,11 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+
+  /* スクリーンショット比較の設定 */
+  expect: {
+    toHaveScreenshot: { threshold: 0.05, maxDiffPixels: 100 },
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
